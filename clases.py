064f254e8d43afe91626cabf06a5d76e68a219cb
Jegs03 <68675624+Jegs03@users.noreply.github.com>
2023-05-28 01:30:40 -0500

    Update clases.py

--- clases.py
+++ clases.py
@@ -3,28 +3,30 @@ import math
 from scipy.spatial import distance
 
 class carro:
-    def __init__(self,id, posicion, carga, capacidad,tipo):
-        self.id=id
+    def __init__(self,cod, posicion, carga, capacidad,tipo,estacion):
+        self.id=cod
         self.posicion=posicion
         self.carga=carga
         self.capacidad=capacidad
         self.tipo=tipo
         self.rango=carga*tipo
         self.cargando=False
+        self.estacion=estacion
+        self.estado='andando'
     def __str__(self):
-        return f"id:{self.id}\npocicion:{self.posicion}\ncarga:{self.carga*100}%\nrango:{self.rango}"
+        return f"id:{self.id}\npocicion:{self.posicion}\ncarga:{self.carga*100}%\nrango:{self.rango}\nestacion:{self.estacion.id}"
     def hacer_fila(self,punto_de_carga):
         punto_de_carga.fila.append(self)
         return len(punto_de_carga.fila)
     def moverse(self):
         old_pos=self.posicion
-        self.posicion=cord_rand(old_pos,self.rango)
+        self.posicion=cord_rand(old_pos,self.rango*0.25)
         self.rango=self.rango - distance.euclidean(self.posicion,old_pos)
         self.carga=self.rango/self.tipo
 
 
     def buscar_cargador(self,ls_cargadores):
-        minimo=ls_cargadores[0]
+        minimo=self.estacion
         for x in ls_cargadores:
             if  distance.euclidean(self.posicion,x.posicion)<=self.rango:
                 if distance.euclidean(self.posicion,x.posicion)<distance.euclidean(self.posicion,minimo.posicion):
@@ -36,7 +38,23 @@ class carro:
                      continue
             else:
                  continue
-        return minimo 
+        self.estacion=minimo
+    def buscar_cargador_rn(self,ls_cargadores):
+        self.estacion=rn.choice(ls_cargadores) 
+    def ir_cargador(self):
+        old_pos=self.posicion
+        self.posicion=self.estacion.posicion
+        self.rango=self.rango - distance.euclidean(self.posicion,old_pos)
+        self.carga=self.rango/self.tipo
+        return distance.euclidean(self.posicion,old_pos)
+    def list_cargador(self,ls_cargadores):
+        ls=[]
+        for x in ls_cargadores:
+            if  distance.euclidean(self.posicion,x.posicion)<=self.rango*0.25:
+                ls.append(x)
+        return ls
+        
+        
                  
                  
                  
@@ -52,6 +70,25 @@ class punto_de_carga:
             self.cargando=[]
         def __str__(self):
             return f"id:{self.id}\npocicion:{self.posicion}\ncarga:{self.carga*100}%\nfila:{self.fila}"
+        def cargar(self,carro):
+            self.fila.append(carro)
+            carro.estado='enfila'
+            return 50
+        def cargar2(self):
+            for x in self.cargando:
+                x.carga=1
+                x.rango=x.carga*x.tipo
+                x.estado='andando'
+                x.moverse()
+                x.buscar_cargador
+                self.carga=self.carga-0.01
+            self.cargando=[]
+            for n in range(self.capacidad):
+                if self.fila:
+                    c=self.fila.pop()
+                    self.cargando.append(c)
+                else:
+                    continue
         
 
 def cord_rand(p,r):
@@ -62,4 +99,14 @@ def cord_rand(p,r):
     x = r * math.cos(alpha) + p[0]
     y = r * math.sin(alpha) + p[1]
     return (x,y)
+
+def crear_carros(n,est,i):
+    carros=[]
+    for x in range(n):
+        cor=cord_rand(est.posicion,2)
+        car=carro(chr(x+97+i),cor,(rn.triangular(0.25, 1,0.75)),10000,50,est)
+        carros.append(car)
+    return carros 
+
+
 
